71787054ac5bd567e2f9aca5a6f6a90884fe418a
Ravi <ravi.enu@gmail.com>
2019-12-15 22:20:35 +1100

    master changes after test1 branch

--- AppsToInstall/src/main/java/com/flexera/application/Compute.java
+++ AppsToInstall/src/main/java/com/flexera/application/Compute.java
@@ -3,7 +3,7 @@ package com.flexera.application;
 import com.flexera.model.User;
 
 import java.util.Map;
-
+//Master changes
 /**
  * This class computes the total number of applications required to install
  */
